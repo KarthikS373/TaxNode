4c6c746005321cb548a62e4bd69e5f1c05526d90
chinmay <chinmay.gargava@gmail.com>
2022-12-01 23:26:55 +0530

    Update txreqtopayu.js

--- src/pages/api/txreqtopayu.js
+++ src/pages/api/txreqtopayu.js
@@ -94,6 +94,7 @@ export default async function handler(req, res) {
       );
       return res.status(200).send({
         status: true,
+        responseUrl: response?.request?.res?.responseUrl,
         message: "Operation Succeeded",
       });
     }
