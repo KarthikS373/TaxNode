2f07bb97ea2d717e53dd9ca7f8672f6000f2fcd2
Prasurjya Pran Borah <96339995+ppborah@users.noreply.github.com>
2022-12-03 15:29:30 +0530

    feat: add /dwnldtaxguide api, upload sample taxguide.pdf

---
+++ src/pages/api/dwnldtaxguide.js
@@ -0,0 +1,65 @@
+import AWS from "aws-sdk";
+import nodemailer from "nodemailer";
+import path from "path";
+
+export default async function handler(req, res) {
+  try {
+    const data = req.query;
+    // Basic Validation
+    if (!(data.name && data.email)) {
+      return res.status(400).send({
+        status: false,
+        message: "Operation Failed",
+      });
+    }
+
+    //TODO: Understand the concepts of port and secure from Chinmay
+    const mailer = nodemailer.createTransport({
+      host: process.env.AWS_SMTP_HOST,
+      port: process.env.AWS_SMTP_PORT || 465,
+      secure: true,
+      auth: {
+        user: process.env.AWS_SMTP_USER,
+        pass: process.env.AWS_SMTP_PASSWORD,
+      },
+    });
+
+    mailer.sendMail(
+      {
+        from: process.env.SMTP_SENDER_EMAIL,
+        to: data.email,
+        subject: process.env.EMAIL_SUBJECT || "No Subject",
+        text: `Hi ${data.name},
+This is a test email from Prasurjya's /dwnldtaxguide API
+Everything is working fine!..............
+
+Regards
+Taxnodes Team`,
+        attachments: [
+          {
+            filename: "taxguide.pdf",
+            path: path.join(process.cwd(), "/assets/pdfs", "taxguide.pdf"),
+          },
+        ],
+      },
+      function (error, info) {
+        if (error) {
+          console.log("Error in sendMail: ", error);
+        } else if (info) {
+          console.log("This is info", info);
+        }
+      }
+    );
+
+    res.status(200).send({
+      status: true,
+      message: "Operation Succeeded",
+    });
+  } catch (error) {
+    console.log("This is in catch block; error.message: ", error.message);
+    res.status(400).send({
+      status: false,
+      message: "Operation Failed",
+    });
+  }
+}
