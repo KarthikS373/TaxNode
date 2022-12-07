b2284baf721718ea6fc5cc7520478cf8418afe43
chinmay <chinmay.gargava@gmail.com>
2022-12-07 13:53:36 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,15 @@ import { theme } from "../components/theme";
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZZG5V5QX89"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZZG5V5QX89');
+        </script>
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
