ac7321cd906fe8623013f1c09e2af7c72d191338
abhinay <abhinay@saaspay.app>
2022-12-21 19:32:07 +0530

    prismic setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 
-import { DefaultSeo } from "next-seo";
+import {DefaultSeo} from "next-seo";
 
-import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import {ChakraProvider} from "@chakra-ui/react";
 
 import Layout from "../components/layout";
 import SEO from "../../next-seo.config";
+import {PrismicProvider} from '@prismicio/react'
+import {PrismicPreview} from '@prismicio/next'
+import {repositoryName} from '../../prismicio'
 
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 import "@fontsource/poppins/800.css";
 import theme from '../../theme'
+import Link from "next/link";
 
 // const theme = extendTheme({
 //   fonts: {
@@ -21,15 +25,18 @@ import theme from '../../theme'
 // });
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <DefaultSeo {...SEO} />
-
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
-  </>
+    <>
+        <DefaultSeo {...SEO} />
+        <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+            <PrismicPreview repositoryName={repositoryName}>
+                <ChakraProvider theme={theme}>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </ChakraProvider>
+            </PrismicPreview>
+        </PrismicProvider>
+    </>
 );
 
 export default MyApp;
