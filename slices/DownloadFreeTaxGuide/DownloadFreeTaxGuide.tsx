1fe07007a203b5b077232efc6a120bd258f3c135
KarthikS373 <lit2021012@iiitl.ac.in>
2022-12-23 20:44:20 +0530

    Validation of home page form

--- slices/DownloadFreeTaxGuide/DownloadFreeTaxGuide.tsx
+++ slices/DownloadFreeTaxGuide/DownloadFreeTaxGuide.tsx
@@ -1,3 +1,7 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import * as Yup from "yup";
+import { useState } from "react";
+import { useFormik } from "formik";
 import {
   Box,
   Button,
@@ -20,8 +24,60 @@ import type { DownloadFreeTaxGuideSlice } from "../../.slicemachine/prismicio";
 const TitleSubtitleCta: React.FC<{ slice: DownloadFreeTaxGuideSlice }> = ({
   slice,
 }) => {
+  const [loading, setLoading] = useState(false);
   const { primary } = slice;
-  const toast = useToast()
+  const toast = useToast();
+
+  // Form Validation and Submission
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+    }),
+    onSubmit: async (value) => {
+      setLoading(true);
+
+      axios
+        .get("/api/dwnldtaxguide", {
+          params: {
+            name: formik.values.name,
+            email: formik.values.email,
+          },
+        })
+        .then((response: AxiosResponse) => {
+          console.log(response);
+          if (response.status == 200) {
+            toast({
+              title: "Account created.",
+              description: primary.success_text,
+              status: "success",
+              duration: 9000,
+              isClosable: true,
+            });
+            formik.resetForm();
+          } else if (response.status == 400) {
+            throw new Error(response.data.message);
+          }
+        })
+        .catch((error: AxiosError) => {
+          console.log(error.message);
+          toast({
+            title: "Sorry we ran into some error",
+            description: primary.error_text,
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+          });
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+  });
 
   return (
     <>
@@ -39,71 +95,83 @@ const TitleSubtitleCta: React.FC<{ slice: DownloadFreeTaxGuideSlice }> = ({
           >
             {primary.title}
           </Heading>
-
-          <Flex
-            display={"flex"}
-            alignItems={"center"}
-            justifyContent={"center"}
-            flexDir={"column"}
-            gap={[3, null, 4, null, 5]}
-          >
+          <form onSubmit={formik.handleSubmit} noValidate>
             <Flex
-              flexDir={{ base: "column", sm: "row" }}
-              alignItems={"flex-start"}
+              display={"flex"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              flexDir={"column"}
               gap={[3, null, 4, null, 5]}
             >
-              <FormControl isRequired flex={1}>
-                <Input
-                  placeholder={primary.name_placeholder}
-                  variant={"secondary"}
-                  size={"lg"}
-                />
-                <FormErrorMessage>{primary.name_error_text}</FormErrorMessage>
-              </FormControl>
-              <FormControl isRequired flex={1}>
-                <Input
-                  type="email"
-                  placeholder={primary.email_placeholder}
-                  variant={"secondary"}
-                  size={"lg"}
-                />
-                <FormErrorMessage>{primary.email_placeholder}</FormErrorMessage>
-              </FormControl>
-            </Flex>
-            <Box pos={"relative"} w={"fit-content"}>
-              <Button
-                variant={"tertiary"}
-                // disabled
-                size={"lg"}
-                w={"fit-content"}
-                rightIcon={
-                  <>
-                    <DownloadIcon />
-                  </>
-                }
-                onClick={() =>
-                  toast({
-                    title: 'Account created.',
-                    description: "We've created your account for you.",
-                    status: 'success',
-                    duration: 9000,
-                    isClosable: true,
-                  })
-                }
+              <Flex
+                flexDir={{ base: "column", sm: "row" }}
+                alignItems={"flex-start"}
+                gap={[3, null, 4, null, 5]}
               >
-                Download FreeTax Guide
-              </Button>
-              <Spinner
-                size={"sm"}
-                pos={"absolute"}
-                top={0}
-                bottom={0}
-                right={"50%"}
-                left={"50%"}
-                margin={"auto"}
-              />
-            </Box>
-          </Flex>
+                <FormControl
+                  isRequired
+                  flex={1}
+                  isInvalid={formik.touched.name && !!formik.errors.name}
+                >
+                  <Input
+                    placeholder={primary.name_placeholder || ""}
+                    variant={"secondary"}
+                    size={"lg"}
+                    name="name"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                  />
+                  <FormErrorMessage>{primary.name_error_text}</FormErrorMessage>
+                </FormControl>
+                <FormControl
+                  isRequired
+                  flex={1}
+                  isInvalid={formik.touched.email && !!formik.errors.email}
+                >
+                  <Input
+                    type="email"
+                    placeholder={primary.email_placeholder || ""}
+                    variant={"secondary"}
+                    size={"lg"}
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                  />
+                  <FormErrorMessage>
+                    {primary.email_error_text}
+                  </FormErrorMessage>
+                </FormControl>
+              </Flex>
+              <Box pos={"relative"} w={"fit-content"}>
+                <Button
+                  variant={"tertiary"}
+                  type="submit"
+                  disabled={loading}
+                  size={"lg"}
+                  w={"fit-content"}
+                  rightIcon={
+                    <>
+                      <DownloadIcon />
+                    </>
+                  }
+                  onClick={() => {}}
+                >
+                  Download FreeTax Guide
+                </Button>
+                {loading && (
+                  <Spinner
+                    size={"sm"}
+                    pos={"absolute"}
+                    top={0}
+                    bottom={0}
+                    right={"50%"}
+                    left={"50%"}
+                    margin={"auto"}
+                  />
+                )}
+              </Box>
+            </Flex>
+          </form>
         </Container>
       </Box>
     </>
